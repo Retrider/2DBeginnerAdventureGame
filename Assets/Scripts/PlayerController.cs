f3c889c5e46b98bce2343816be078842a0b4cb48
Retrider <a55155@alunos.ipb.pt>
2024-06-23 17:29:55 +0100

    Make your game frame rate independent

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,15 +15,14 @@ public class PlayerController : MonoBehaviour
         MoveAction.Enable();
     }
 
-
     // Update is called once per frame
     void Update()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
+
+
 }
-
-
